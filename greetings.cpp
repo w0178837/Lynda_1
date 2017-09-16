69214b591ce050402ae25a0cc2a40ee88cabc4e1
w0178837 <N4s6c4c2>
2017-09-16 11:52:04 -0300

    Finished chapter 2 of Become a C++ Developer with Peggy Fischer
    - learned implicit/explicit conversion of datatypes

---
+++ greetings.cpp
@@ -0,0 +1,14 @@
+//
+// Created by inet2005 on 9/16/17.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // todo insert code here
+    return 0;
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+/*
+asciitable.cpp
+May 20, 2014
+Peggy Fisher
+*/
 #include <iostream>
+using namespace std;
+int main()
+{
+    int dollars, cents;
+    double price;
+    char dollarsign = 36; //implicit conversion
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout<<"Enter the price in dollars and cents"<<endl;
+    cin>>price;
+    dollars = price; // implicitly inserts dollar portion to the dollars variable as an int
+    cents = (price - dollars) * 100; // gives us the cent value truncated to the integer value left of the decimal place after the calculation
+
+    cout<<"Dollars: "<<dollarsign<<dollars<<" cents: "<<cents<<endl;
     return 0;
 }
